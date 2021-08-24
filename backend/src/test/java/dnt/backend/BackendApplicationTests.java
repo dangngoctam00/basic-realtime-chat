7751c082ef4df2b6e439a4dffec8fc38dcd96a0c
Dang Ngoc Tam <dangngoctam00@gmail.com>
2021-08-25 00:10:41 +0700

    initial

---
+++ backend/src/test/java/dnt/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package dnt.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
