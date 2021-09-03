96dee9cd150001c41a7140bc1993c775a02a18f5
Dang Ngoc Tam <dangngoctam00@gmail.com>
2021-09-04 01:01:46 +0700

    use embedded h2-database and add initial data

---
+++ backend/src/main/resources/data.sql
@@ -0,0 +1,18 @@
+INSERT INTO `USERS`(`USERNAME`)
+VALUES
+    ('dnt00'),
+    ('dnt01'),
+    ('dnt02'),
+    ('dnt03'),
+    ('dnt04');
+
+INSERT INTO `FRIEND_SHIP`(`ME`, `YOU`)
+VALUES
+    ('dnt00', 'dnt01'),
+    ('dnt01', 'dnt00'),
+    ('dnt00', 'dnt02'),
+    ('dnt02', 'dnt00'),
+    ('dnt00', 'dnt03'),
+    ('dnt03', 'dnt00'),
+    ('dnt01', 'dnt02'),
+    ('dnt02', 'dnt01');
